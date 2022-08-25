5c02457477198a253778b013d445208f17698cff
OviyaJ1 <75902846+OviyaJ1@users.noreply.github.com>
2022-08-25 13:03:53 -0400

    Add files via upload
    "Completed"

--- l020_PythonExercises2.py
+++ l020_PythonExercises2.py
@@ -1,22 +1,50 @@
+#Oviya Jeyaprakash 08/25/2022
+#Python Exercises 2
+#020
+
 import sys
 s = sys.argv[1]
 
-print(s[2]) #1
+print("#1:", s[2])
 
-print(s[4]) #2
+print("#2:", s[4])
 
-print(len(s)) #3
+print("#3:", len(s))
 
-print(s[0]) #4
+print("#4:", s[0])
 
-print(s[len(s) - 1]) #5
+print("#5:", s[len(s) - 1])
 
-print(s[len(s) - 2]) #6
+print("#6:", s[len(s) - 2])
 
-print(s[3:8]) #7
+print("#7:", s[3:8])
 
-print(s[len(s) - 5:]) #8
+print("#8:", s[len(s) - 5:])
 
-print(s[3:]) #9
+print("#9:", s[2:])
 
-print(value for index, value in enumerate(s) if index%2 == 1) #10!
+print("#10:", s[::2])
+
+print("#11:", s[1::3])
+
+print("#12:", s[::-1])
+
+print("#13:", s.find(" "))
+
+print("#14:", s[:len(s) - 1])
+
+print("#15:", s[1:])
+
+print("#16:", s.lower())
+
+print("#17:", (x := s.split()))
+
+print("#18:", len(x))
+
+print("#19:", list(s))
+
+print("#20:", ''.join(sorted(s)))
+
+print("#21:", s[:s.find(" ")])
+
+print("#22:", True if s == s[::-1] else False)
